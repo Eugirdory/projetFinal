6831072b872808ec17ba2b1c5ff13ad560917247
sarahDLB <dlbsarah93@hotmail.fr>
2019-07-08 14:42:38 +0200

    projetDoki

---
+++ projetDoki/src/main/java/formation/sopra/bu/projetDoki/ProjetDokiApplication.java
@@ -0,0 +1,13 @@
+package formation.sopra.bu.projetDoki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDokiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDokiApplication.class, args);
+	}
+
+}
