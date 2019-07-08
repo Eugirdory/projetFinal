0efc7044694c9ffda653345a7f8458aa204670b7
Rodrigue <sy.rodrigue@gmail.com>
2019-07-08 16:33:02 +0200

    jsonviews

---
+++ projetDoki/src/main/java/formation/sopra/bu/projetDoki/view/JsonViews.java
@@ -0,0 +1,23 @@
+package formation.sopra.bu.projetDoki.view;
+
+public class JsonViews {
+	public static class Common {
+
+	};
+
+	public static class PraticienAvecRdv extends Common {
+
+	};
+
+	public static class RdvAvecPraticien extends Common {
+
+	};
+
+	public static class PatientAvecRdv extends Common {
+
+	};
+	
+	public static class RdvAvecPatient extends Common {
+
+	};
+}
