581d0454c318f0ec6496b7e38d5fe7bf5741b4de
Rodrigue <sy.rodrigue@gmail.com>
2019-07-08 16:25:59 +0200

    spe change

--- projetDoki/src/main/java/formation/sopra/bu/projetDoki/metier/Specialite.java
+++ projetDoki/src/main/java/formation/sopra/bu/projetDoki/metier/Specialite.java
@@ -1,5 +1,7 @@
 package formation.sopra.bu.projetDoki.metier;
 
+import java.util.List;
+
 import javax.persistence.*;
 
 @Entity
@@ -13,8 +15,20 @@ public class Specialite {
 	@Column(name = "libele")
 	private String libele;
 	
+	@OneToOne
+	@JoinColumn(name = "id_praticien")
+	private Praticien praticien;
+	
+	@OneToMany(mappedBy = "key.specialite")
+	@Column(name="praticien")
+	private List<PraticienSpe> praticienSpe;
+	
+	
+	//Constructor
+	
 	public Specialite() {
 	}
+	
 	public Specialite(String libele) {
 		this.libele = libele;
 	}
@@ -23,20 +37,41 @@ public class Specialite {
 		this.id = id;
 		this.libele = libele;
 	}
+	
+	//Getter et Setter
 	public String getLibele() {
 		return libele;
 	}
 	public void setLibele(String libele) {
 		this.libele = libele;
 	}
+	public Praticien getPraticien() {
+		return praticien;
+	}
+
+	public void setPraticien(Praticien praticien) {
+		this.praticien = praticien;
+	}
+
+	public List<PraticienSpe> getPraticienSpe() {
+		return praticienSpe;
+	}
+
+	public void setPraticienSpe(List<PraticienSpe> praticienSpe) {
+		this.praticienSpe = praticienSpe;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((libele == null) ? 0 : libele.hashCode());
+		result = prime * result + ((praticien == null) ? 0 : praticien.hashCode());
+		result = prime * result + ((praticienSpe == null) ? 0 : praticienSpe.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -56,8 +91,20 @@ public class Specialite {
 				return false;
 		} else if (!libele.equals(other.libele))
 			return false;
+		if (praticien == null) {
+			if (other.praticien != null)
+				return false;
+		} else if (!praticien.equals(other.praticien))
+			return false;
+		if (praticienSpe == null) {
+			if (other.praticienSpe != null)
+				return false;
+		} else if (!praticienSpe.equals(other.praticienSpe))
+			return false;
 		return true;
 	}
+
+	
 	
 	
 }
