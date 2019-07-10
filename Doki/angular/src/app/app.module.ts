75e85f861d60490b1d77cbed59d930ef3a801854
Rodrigue <sy.rodrigue@gmail.com>
2019-07-10 14:18:55 +0200

    modut praticien

--- Doki/angular/src/app/app.module.ts
+++ Doki/angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {LoginComponent} from './login/login.component';
 import {BarreRechercheComponent} from './barre-recherche/barre-recherche.component';
 import {AuthenticationService} from './services/authentication.service';
 import {PraticienService} from './services/praticien-service';
+import { PraticienComponent } from './praticien/praticien.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import {PraticienService} from './services/praticien-service';
     InscriptionComponent,
     PageAcceuilComponent,
     LoginComponent,
-    BarreRechercheComponent
+    BarreRechercheComponent,
+    PraticienComponent
 
   ],
   imports: [
