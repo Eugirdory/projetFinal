13139cfaf5ed8eb0bc789db40d9ce733f73a9f3d
Rodrigue <sy.rodrigue@gmail.com>
2019-07-10 13:51:24 +0200

    module avec service

--- Doki/angular/src/app/app.module.ts
+++ Doki/angular/src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {routes} from './routes';
-import { PatientComponent } from './patient/patient.component';
+import {PatientComponent} from './patient/patient.component';
 import {MenuComponent} from './menu/menu.component';
-import { InscriptionComponent } from './inscription/inscription.component';
-import { PageAcceuilComponent } from './page-acceuil/page-acceuil.component';
-import { LoginComponent } from './login/login.component';
-import { BarreRechercheComponent } from './barre-recherche/barre-recherche.component';
+import {InscriptionComponent} from './inscription/inscription.component';
+import {PageAcceuilComponent} from './page-acceuil/page-acceuil.component';
+import {LoginComponent} from './login/login.component';
+import {BarreRechercheComponent} from './barre-recherche/barre-recherche.component';
+import {AuthenticationService} from './services/authentication.service';
+import {PraticienService} from './services/praticien-service';
 
 
 @NgModule({
@@ -21,14 +23,15 @@ import { BarreRechercheComponent } from './barre-recherche/barre-recherche.compo
     MenuComponent,
     InscriptionComponent,
     PageAcceuilComponent,
-    LoginComponent
+    LoginComponent,
     BarreRechercheComponent
 
   ],
   imports: [
     BrowserModule, FormsModule, RouterModule.forRoot(routes), HttpClientModule
   ],
-  providers: [],
+  providers: [AuthenticationService, PraticienService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
