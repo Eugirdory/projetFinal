50d42775c028e47bbd066039b6adffa050719308
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-07-10 13:59:46 +0200

    login

--- Doki/angular/src/app/login/login.component.ts
+++ Doki/angular/src/app/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Personne} from '../model/personne';
+import {AuthenticationService} from '../services/authentication.service';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private personne: Personne = new Personne();
+  private message = '';
+
+
+  constructor(private authService: AuthenticationService) {
+  }
 
   ngOnInit() {
   }
 
+  public login() {
+    if (this.authService.login(this.personne)) {
+      console.log('ok');
+    } else {
+      this.message = 'erreur authentification';
+    }
+
+  }
 }
