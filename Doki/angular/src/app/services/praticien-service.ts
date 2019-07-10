7c41b4ebebfbc695b555681bb59849271fafe3a8
Rodrigue <sy.rodrigue@gmail.com>
2019-07-10 13:50:27 +0200

    praticien service

--- Doki/angular/src/app/services/praticien-service.ts
+++ Doki/angular/src/app/services/praticien-service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {url} from 'inspector';
 import {Praticien} from '../model/praticien';
 
 @Injectable({providedIn: 'root'})
