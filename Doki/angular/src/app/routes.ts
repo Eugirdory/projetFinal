1e8bb2cdd62152e42fcdab9d0a3519d081845217
Rodrigue <sy.rodrigue@gmail.com>
2019-07-10 11:08:43 +0200

    route accueuil

--- Doki/angular/src/app/routes.ts
+++ Doki/angular/src/app/routes.ts
@@ -1,8 +1,9 @@
 import {Routes} from '@angular/router';
 import {InscriptionComponent} from './inscription/inscription.component';
+import {PageAcceuilComponent} from './page-acceuil/page-acceuil.component';
 
 
 export const routes: Routes = [
   {path: 'inscription', component: InscriptionComponent},
-
+  {path: 'acceuil', component : PageAcceuilComponent},
 ];
