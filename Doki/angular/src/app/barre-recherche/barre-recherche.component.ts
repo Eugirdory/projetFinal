cab2077f7489ffa33d2b88a650b166961a77cb44
Rodrigue <sy.rodrigue@gmail.com>
2019-07-10 13:54:09 +0200

    correction service et recherche pract

--- Doki/angular/src/app/barre-recherche/barre-recherche.component.ts
+++ Doki/angular/src/app/barre-recherche/barre-recherche.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-barre-recherche',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarreRechercheComponent implements OnInit {
 
-  constructor() { }
+  private mot: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  public searchAction() {
+
   }
 
 }
