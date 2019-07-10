ce44e55374660f5743583aa5223e756ee51fad1a
sarahDLB <dlbsarah93@hotmail.fr>
2019-07-10 14:00:32 +0200

    praticien

--- Doki/angular/src/app/model/praticien.ts
+++ Doki/angular/src/app/model/praticien.ts
@@ -4,21 +4,33 @@ import {Specialite} from './specialite';
 import {Disponibilite} from './disponibilite';
 import {Personne} from './personne';
 import {Civilite} from './civilite.enum';
+import {Adresse} from './adresse';
 
 export class Praticien extends Personne{
 
+  private _cabinet: Adresse;
   private _specialites: Array<Specialite> = new Array<Specialite>();
   private _dispos: Array<Disponibilite> = new Array<Disponibilite>();
   private _rdvs: Array<RendezVous> = new Array<RendezVous>();
   private _motifs: Array<Motif> = new Array<Motif>();
 
 
-  constructor(civilite?: Civilite, password?: string, username?: string, nom?: string, prenom?: string, mail?: string, telephone?: string, specialites?: Array<Specialite>, dispos?: Array<Disponibilite>, rdvs?: Array<RendezVous>, motifs?: Array<Motif>) {
+  constructor(civilite?: string, password?: string, username?: string, nom?: string, prenom?: string, mail?: string, telephone?: string, cabinet?: Adresse, specialites?: Array<Specialite>, dispos?: Array<Disponibilite>, motifs?: Array<Motif>, rdvs?: Array<RendezVous>) {
     super(civilite, password, username, nom, prenom, mail, telephone);
+    this._cabinet = cabinet;
     this._specialites = specialites;
     this._dispos = dispos;
     this._rdvs = rdvs;
     this._motifs = motifs;
+  }
+
+
+  get cabinet(): Adresse {
+    return this._cabinet;
+  }
+
+  set cabinet(value: Adresse) {
+    this._cabinet = value;
   }
 
   get specialites(): Array<Specialite> {
