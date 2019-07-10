a10656015c3469f89495aad7b7659fef941e33b6
sarahDLB <dlbsarah93@hotmail.fr>
2019-07-10 11:39:33 +0200

    routes

--- Doki/angular/src/app/routes.ts
+++ Doki/angular/src/app/routes.ts
@@ -1,8 +1,9 @@
 import {Routes} from '@angular/router';
+import {InscriptionPraticienComponent} from './inscription-praticien/inscription-praticien.component';
 
 
 
 export const routes: Routes = [
 
-
+  {path: 'inscriptionPraticien', component: InscriptionPraticienComponent},
 ]
