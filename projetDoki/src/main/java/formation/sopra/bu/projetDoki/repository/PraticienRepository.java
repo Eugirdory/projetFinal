ce3e8f98920924c43e894b125b3fa386e19e0857
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-07-11 09:15:08 +0200

    query FindByVilleBySpecialite FINIIIIIIIIIIIIIIIIIIEEEEEEEEEEEEE§

--- projetDoki/src/main/java/formation/sopra/bu/projetDoki/repository/PraticienRepository.java
+++ projetDoki/src/main/java/formation/sopra/bu/projetDoki/repository/PraticienRepository.java
@@ -26,9 +26,11 @@ public interface PraticienRepository extends JpaRepository<Praticien, String> {
 //OK
 	@Query("select distinct p from Praticien p left join fetch p.dispos as d left join fetch d.cabinet as c where c.ville=:ville")
 	Optional<Praticien> findAllByVille(@Param("ville") String ville);
-
-	// findByVilleAndSpecialite
-//	@Query("select distinct p from Praticien p left join fetch p.specialites as ss left join fetch ss.key as k left join fetch k.specialite as s left join fetch s.libelle left join fetch p.dispos as d left join fetch d.cabinet as c where c.ville=:ville")
-//	Optional<Praticien> FindAllWithSpecialiteAndVille(@Param("libelle") String libelle, @Param("ville") String ville);
+	
+//OK
+	@Query("select distinct p from Praticien p inner join p.dispos as d inner join d.cabinet as c "
+			+ "inner join p.specialites as ss inner join ss.key as k inner join k.specialite as s "
+			+ "where c.ville=:ville and s.libelle=:libelle")
+	Optional<Praticien> FindAllWithVilleAndSpecialite(@Param("ville") String ville, @Param("libelle") String llibelle);
 
 }
