cb329fc99986e208aecc7c4c16e14c21fd251d49
sarahDLB <dlbsarah93@hotmail.fr>
2019-07-11 14:55:11 +0200

    service patient

---
+++ Doki/angular/src/app/services/patient.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Praticien} from '../model/praticien';
+import {Patient} from '../model/patient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientService {
+  private url = 'http://localhost:8080/boot/rest/praticien';
+  private httpHeaders: HttpHeaders;
+  private httpOptions: any;
+
+  constructor(private http: HttpClient) {
+    this.httpHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'No'   + btoa('moi:toto')
+    });
+    this.httpOptions = {headers: this.httpHeaders};
+  }
+  public list(): Observable<any> {
+    return this.http.get(this.url, {headers: this.httpHeaders});
+  }
+
+  public delete(id: string): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`, this.httpOptions);
+  }
+
+  public update(patient: Patient): Observable<any> {
+    return this.http.put(`${this.url}/${patient.username}`, patient, this.httpOptions);
+  }
+
+  public insert(patient: Patient): Observable<any> {
+    const p = {
+      'mail': patient.mail,
+      'username': patient.username,
+      'password': patient.password,
+      'civilite': patient.civilite,
+      'nom': patient.nom,
+      'prenom': patient.prenom,
+      'telephone': patient.telephone,
+      'adresse': patient.domicile,
+      'dateNaissance': patient.dateNaissance,
+    };
+    return this.http.post(`${this.url}`, p, this.httpOptions);
+  }
+}
